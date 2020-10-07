ad6b959a9d84c8738cad7cfcd4b4d8f44b401a27
Jeevan Bhandari <72492753+bhandarijeevan@users.noreply.github.com>
2020-10-07 18:37:50 +0545

    create new project

---
+++ Hello-World/Arithematic_operator.c
@@ -0,0 +1,43 @@
+//* write a simple program using Arithmetic operator *//
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+// tell the compiler the start the program
+int main()
+{
+    // declaration the necessary variables
+    int num1, num2;
+    int sum, sub, mul, div, mod;
+
+    printf("\n Enter the first numbers:");
+    scanf("%d",&num1);
+
+    printf("\n Enter the second numbers:");
+    scanf("%d",&num2);
+    // calculate sum
+    sum=num1+num2;
+    printf("\n The sum is %d", sum);
+
+   // calculate subtraction
+    sub=num1-num2;
+    printf("\n The subtraction is %d", sub);
+
+    // calculate multiplication
+    mul=num1*num2;
+    printf("\n The multiplication is %d", mul);
+
+    // calculate division
+    div=num1/num2;
+    printf("\n The division is %d",div);
+
+    // calculate Reminder/ modulo
+    mod=num1%num2;
+    printf("\n The Reminder divided by num2 is %d", mod);
+
+
+    getch();
+    return 0;
+
+}
